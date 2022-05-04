895c1ca1a44e5f3e069ca27b86a55825b4dcb23c
Balbir Singh <70421976+bs-cse-iitb@users.noreply.github.com>
2022-05-04 17:05:39 +0530

    Update Insertion_Sort.cpp

--- Sorting/Insertion_Sort.cpp
+++ Sorting/Insertion_Sort.cpp
@@ -5,10 +5,10 @@ void insertionSort(int arr[], int n)
 	for(i=1;i<n;i++){
 		temp = arr[i];
 		j=i-1;
-		while(j>0&& arr[j]>temp){ // 
+		while(j>=0&& arr[j]>temp){ // 
 			arr[j+1]=arr[j];
 			j--;
 		}
-		arr[j-1]=temp; // j+1, not j-1
+		arr[j+1]=temp; // j+1, not j-1
 	}
 }
